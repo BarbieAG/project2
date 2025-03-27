16f81bf07da65229f1e2c717eae4d5602563d69c
Barbara Griffin <bag155@miami.edu>
2025-03-27 12:58:01 -0400

    improving game, it's not good but better than before

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
 
-let step = 2
-let playerLife = 3; 
+let catX= 0
+let catY = 0
+let step = 0
 let score = 0;
-
-
+let life = 1
 let enemies = []
+let enemiesRows = []
 let treats = []
 
 
+// function preload(){
+//     loadFont("https://fonts.googleapis.com/css2?family=Jacquard+12&display=swap")
+// }
 
 function setup(){
     createCanvas(800,600);
@@ -18,34 +22,49 @@ function setup(){
             x:random(20, 750),
             y:0,  
             step: random(1, 8)
+            // stepX: random(1, 8)
         }
         enemies.push(enemy)
     }
 
-    for(let i=0; i<8; i++){
-        let treat ={
+    for(let i =0; i<5; i++){
+        let enemyRow = {
+            x:0,
+            y:random(20, 580),
+            step: random(1,8)
+        }
+        enemiesRows.push(enemyRow)
+    }
+
+    for(let i=0 ; i<10; i++){
+        let treat ={ 
             x:random(10, 780),
             y:random(10, 580)
+
         }
         treats.push(treat)
     }
    
 }
 function draw(){
+  
   background("black")
   drawEnemies()
+  drawEnemiesRows()
   fill("yellow")
   drawTreats()
   fill("orange")
-  square(mouseX * 2,  mouseY * 2, 20)
-  fill("white")
-  square(760,560,20)
-  
-  
+  cat()
+  win()
+  gameOver()
+}
+
+function cat(){
+    catX = catX + (mouseX - catX) * .06
+    catY = catY + (mouseY - catY) * .06
+    square(catX, catY, 20)
 }
  
-
-
 function drawEnemies(){
     
     for (let enemy of enemies){
@@ -61,28 +80,98 @@ function drawEnemies(){
         enemy.y += enemy.step
        
         square(enemy.x, enemy.y, 20)
-        let distance = dist(enemy.x, enemy.y, mouseX * 1, mouseY * 1)
-        if (distance < 5){
-            playerLife -= 1
-        }
-        // console.log(distance, playerLife)
 
-        
-    }    
+        let distance = dist(enemy.x, enemy.y, catX, catY)
+        if (distance < 20){
+            life--
+        //     score--
+        //     playerScore.textContent = score  
+
+        }    
     
     
+    }
+}
+
+function drawEnemiesRows(){
+   
+    for( let enemyRow of enemiesRows){
+        fill("blue")
+        if (enemyRow.x > 750){
+            enemyRow.step = enemyRow.step * -1
+        }
+        else if (enemyRow.x < 0){
+            enemyRow.step = enemyRow.step * -1
+        }
+
+        enemyRow.x += enemyRow.step
+        square(enemyRow.x, enemyRow.y, 20)
+
+        let Horizontaldistance = dist(enemyRow.x, enemyRow.y, catX, catY)
+        if (Horizontaldistance < 20){
+           life--
+            // score--
+            // playerScore.textContent = score
+            
+        }
+
+    //     else if (score < 0){
+    //         playerScore.textContent = 0
+    //     }
+    // }
+    }
 }
 
 function drawTreats(){
-    for(let treat of treats){
+    for(let i= treats.length -1; i >= 0; i--){
+        let treat = treats[i]
         circle(treat.x, treat.y, 20)
-        let treatDistance = dist( treat.x, treat.y, mouseX*0.8, mouseY*0.8)
+        let treatDistance = dist( treat.x, treat.y, catX, catY)
         if (treatDistance < 20){
-            score++;
+            score++
             playerScore.textContent = score
-           
+            treats.splice(i,1)
+        
         }
-        console.log(score, treatDistance)
+        // console.log(score, treatDistance)
+            
 
     }
+    
 }
+
+function win(){
+    fill("white")
+     square(760,560,20)
+    
+    let winDistance = dist(760, 560, catX, catY)
+    if (winDistance < 20){
+        background("black")
+        fill("yellow")
+        textAlign("center")
+        textSize(45)
+        text("Congrats!!", 400, 300)
+        let a = createA("./index.html", "Restart")
+        a.position(420,400)
+        noLoop()
+    }
+
+   
+}
+
+function gameOver(){
+    if (life ==0){
+        background("black")
+        fill("red")
+        textSize(45)
+        text("Game Over :(", 300,300)
+        let a = createA("./index.html", "Restart")
+        a.position(420,400)
+        noLoop()
+
+    }
+       
+       
+}
+
+
